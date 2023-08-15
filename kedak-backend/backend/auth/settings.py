813c3499496db4609f236d10ee8c18a134ba8bf4
ayomidetobi <ayomidestobsy10@gmail.com>
2023-08-15 02:10:07 +0100

    api

--- kedak-backend/backend/auth/settings.py
+++ kedak-backend/backend/auth/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'corsheaders',
+    # 'corsheaders',
     'djoser',
     'social_django',
     'rest_framework',
